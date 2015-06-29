d9969d871bf8ba38ff167d9287c6aa306f66d6dd
= <=>
2015-06-28 19:47:16 -0500

    Further changes to fully support ingestion of configuration file

--- src/main/scala/DamageMeter.scala
+++ src/main/scala/DamageMeter.scala
@@ -3,60 +3,52 @@
  */
 import scala.swing._
 
-class DamageMeter {
+class DamageMeter(windowPeriods: String) {
 
+  // Constant declarations
+  val RollingWindowDamageDescription = " seconds: "
   val CurrentDamageDescription = "Current Fight"
-  val FiveSecondDamageDescription  = "5 seconds: "
-  val TenSecondDamageDescription = "10 seconds: "
-  val TwentySecondDamageDescription  = "20 seconds: "
-  val FortySecondDamageDescription= "40 seconds: "
 
+  val currentDamageLabel = new Label{text = CurrentDamageDescription; foreground = java.awt.Color.WHITE } // This label will be added to primary frame; value of text property will be updated when new data is available
   var currentDamageOutput = 0.0
-  var fiveSecondDamageOutput = 0.0
-  var tenSecondDamageOutput = 0.0
-  var twentySecondDamageOutput = 0.0
-  var fortySecondDamageOutput = 0.0
 
-  val currentDamageLabel = new Label{text = CurrentDamageDescription + currentDamageOutput; foreground = java.awt.Color.WHITE }
-  val fiveSecondDamageLabel = new Label{text = FiveSecondDamageDescription + fiveSecondDamageOutput; foreground = java.awt.Color.WHITE }
-  val tenSecondDamageLabel = new Label{text = TenSecondDamageDescription + tenSecondDamageOutput; foreground = java.awt.Color.WHITE }
-  val twentySecondDamageLabel = new Label{text = TwentySecondDamageDescription + twentySecondDamageOutput; foreground = java.awt.Color.WHITE }
-  val fortySecondDamageLabel = new Label{text = FortySecondDamageDescription + fortySecondDamageOutput; foreground = java.awt.Color.WHITE }
+  val windowPeriodsArray = windowPeriods.split(",")
+  var windowPeriodsLabels = new Array[Label](windowPeriodsArray.length)
+  for (i <- 0 to windowPeriodsArray.length - 1 ) {
+    windowPeriodsLabels(i) = new Label{text = windowPeriodsArray(i) + RollingWindowDamageDescription; foreground = java.awt.Color.WHITE } // Each label will be added to primary frame; value of text property will be updated when new data is available
+  }
 
+  // This MainFrame is the primary frame representing the DamageMeter itself
   val mainFrame = new MainFrame {
     title = "EQLogParser - DamageMeter"
     contents = new GridPanel(1,2) {
       contents += currentDamageLabel
       background = new Color(40,0,0)
-      contents += new GridPanel(4,1) {
-        contents += fiveSecondDamageLabel
-        contents += tenSecondDamageLabel
-        contents += twentySecondDamageLabel
-        contents += fortySecondDamageLabel
+      contents += new GridPanel(windowPeriodsArray.length,1) {
+        for (period <- windowPeriodsLabels ) {
+          contents += period
+        }
         background = new Color(40,0,0)
       }
-      iconImage = toolkit.getImage("assets\\eq_logo.png")
+      iconImage = toolkit.getImage("src\\main\\resources\\eq_logo.png")
     }
     size = new Dimension(500,300)
     centerOnScreen
   }
 
+  /**
+   * Re-calculate all damage values and update label text property values with new information
+   */
   def updateOutputValues(): Unit = {
     currentDamageLabel.text = "<html><body style='text-align:center'>" + CurrentDamageDescription + "<br/><br/><div style='font-size:56px'>" + currentDamageOutput + "</div></body></html>"
-    fiveSecondDamageLabel.text = "<html><body>" + FiveSecondDamageDescription + "<span style='font-size:14px'>" + fiveSecondDamageOutput + "</span></body></html>"
-    tenSecondDamageLabel.text = "<html><body>" + TenSecondDamageDescription + "<span style='font-size:14px'>" + tenSecondDamageOutput + "</span></body></html>"
-    twentySecondDamageLabel.text = "<html><body>" + TwentySecondDamageDescription + "<span style='font-size:14px'>" + twentySecondDamageOutput + "</span></body></html>"
-    fortySecondDamageLabel.text = "<html><body>" + FortySecondDamageDescription + "<span style='font-size:14px'>" + fortySecondDamageOutput + "</span></body></html>"
+    //fiveSecondDamageLabel.text = "<html><body>" + FiveSecondDamageDescription + "<span style='font-size:14px'>" + fiveSecondDamageOutput + "</span></body></html>"
   }
 
+  /**
+   * Display the DamageMeter and begin it's execution
+   */
   def launch(): Unit = {
     updateOutputValues()
     mainFrame.visible = true
-    currentDamageOutput = 20.4
-    fiveSecondDamageOutput = 22.7
-    tenSecondDamageOutput = 22.8
-    twentySecondDamageOutput = 21.3
-    fortySecondDamageOutput = 19.6
-    updateOutputValues()
   }
 }
--- src/main/scala/EQLogParser.scala
+++ src/main/scala/EQLogParser.scala
@@ -1,9 +1,23 @@
+import java.io._
+import java.util.Properties
+import scala.collection.JavaConverters._
+
 /**
  * Created by Taylor Ellison on 6/28/2015.
  */
 object EQLogParser {
+
   def main(args: Array[String]): Unit = {
-    val dm = new DamageMeter();
-    dm.launch()
+
+    // Read in configuration information from the properties file, and store in a Properties object
+    val propertiesFile = new File("src\\main\\config\\EQLogParser.properties")
+    val inputStream = new InputStreamReader(new FileInputStream(propertiesFile), "UTF-8")
+    val properties = new Properties()
+    properties.load(inputStream)
+
+    // Create and launch a new DamageMeter object
+    val damageMeter = new DamageMeter(properties.getProperty("damage-meter-window-periods"));
+    damageMeter.launch()
+
   }
 }
