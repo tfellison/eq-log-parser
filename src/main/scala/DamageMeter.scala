e17457c0812cbb6f4af239a4f21158d3ac275350
= <=>
2015-07-12 15:21:13 -0500

    Add new line processing and output updating

--- src/main/scala/DamageMeter.scala
+++ src/main/scala/DamageMeter.scala
@@ -1,16 +1,20 @@
 /**
  * Created by Taylor Ellison on 6/28/2015.
  */
+
+import java.sql.Timestamp
+
 import scala.swing._
 import scala.sys.process._
 import scala.util.matching.Regex
 import java.time
 
 
-class DamageMeter(windowPeriods: String) {
+class DamageMeter(windowPeriods: String, currentFightDelay: Int) {
 
   private val RollingWindowDamageDescription = " seconds: "
   private val CurrentDamageDescription = "Current Fight"
+
   private val currentDamageLabel = new Label{text = CurrentDamageDescription; foreground = java.awt.Color.WHITE } // This label will be added to primary frame; value of text property will be updated when new data is available
 
   private var currentDamageOutput = 0.0
@@ -18,7 +22,7 @@ class DamageMeter(windowPeriods: String) {
   private val windowPeriodsArray = windowPeriods.split(",")
   private var windowPeriodsMap:Map[Int,Label] = Map()
   for (i <- 0 to windowPeriodsArray.length - 1 ) {
-    windowPeriodsMap += (windowPeriodsArray(i).toInt -> new Label{text = windowPeriodsArray(i) + RollingWindowDamageDescription + 0.0; foreground = java.awt.Color.WHITE })
+    windowPeriodsMap += (windowPeriodsArray(i).toInt -> new Label{text = "<html><body>" + windowPeriodsArray(i) + RollingWindowDamageDescription  + "<br/><br/>span style='font-size:14px'>" + 0.0 + "</span></body></html>"; foreground = java.awt.Color.WHITE })
   }
 
   // This MainFrame is the primary frame representing the DamageMeter itself
@@ -39,15 +43,41 @@ class DamageMeter(windowPeriods: String) {
     centerOnScreen()
   }
 
+  private var damageRecords = scala.collection.mutable.Map[Long,Int]()
+  private val damagePattern = "(?:You|non-melee|taken)(?:\\s|\\w)*?(\\d+)(?:\\s|\\w)*damage".r
+  private var damageCache:Int = 0
+  private val currentFightStart:Long = 0
+
   def processNewLine(line: String): Unit = {
-    println(currentDamageOutput)
+
+    val currentTimestamp: Long = System.currentTimeMillis / 1000
+
+    for(matchDamage <- damagePattern.findAllIn(line).matchData) {
+      damageCache += matchDamage.group(1).toInt
+    }
   }
 
   /**
    * Re-calculate all damage values and update label text values with new information
    */
   private def updateOutputValues(): Unit = {
-    currentDamageLabel.text = "<html><body style='text-align:center'>" + CurrentDamageDescription + "<br/><br/><div style='font-size:56px'>" + currentDamageOutput + "</div></body></html>"
+    val currentTimestamp: Long = System.currentTimeMillis / 1000
+
+    if (damageCache > 0)
+      damageRecords.put(currentTimestamp, damageCache)
+
+    damageCache = 0
+
+    for (period <- windowPeriodsArray) {
+      val periodDamageRecords = damageRecords.filterKeys(currentTimestamp - _ < period.toInt)
+      var sum = 0
+      for (damage <- periodDamageRecords.values) {
+        sum += damage
+      }
+      windowPeriodsMap(period.toInt).text = "<html><body>" + period + RollingWindowDamageDescription + "<span style='font-size:14px'>" + (sum/period.toDouble) + "</span></body></html>"
+    }
+
+    //currentDamageLabel.text = "<html><body style='text-align:center'>" + CurrentDamageDescription + "<br/><br/><div style='font-size:56px'>" + currentDamageOutput + "</div></body></html>"
     //fiveSecondDamageLabel.text = "<html><body>" + FiveSecondDamageDescription + "<span style='font-size:14px'>" + fiveSecondDamageOutput + "</span></body></html>"
   }
 
@@ -57,5 +87,17 @@ class DamageMeter(windowPeriods: String) {
   def launch(): Unit = {
     updateOutputValues()
     mainFrame.visible = true
+
+    val updateThread = new Thread(new Runnable {
+      def run() {
+        while(true) {
+          Thread.sleep(1000)
+          updateOutputValues()
+        }
+      }
+    })
+
+    updateThread.start
+
   }
 }
--- src/main/scala/EQLogParser.scala
+++ src/main/scala/EQLogParser.scala
@@ -1,6 +1,8 @@
 import java.io._
 import java.util.Properties
 import scala.collection.JavaConverters._
+import org.apache.commons.io.input.Tailer
+import org.apache.commons.io.input.TailerListener
 
 /**
  * Created by Taylor Ellison on 6/28/2015.
@@ -15,9 +17,14 @@ object EQLogParser {
     val properties = new Properties()
     properties.load(inputStream)
 
+    val logFile = new File(properties.getProperty("everquest-install-directory") + "Logs\\eqlog_" + properties.getProperty("character-name") + "_project1999.txt")
+
     // Create and launch a new DamageMeter object
-    val damageMeter = new DamageMeter(properties.getProperty("damage-meter-window-periods"));
+    val damageMeter = new DamageMeter(windowPeriods = properties.getProperty("damage-meter-window-periods"), currentFightDelay = properties.getProperty("current-fight-delay").toInt)
     damageMeter.launch()
 
+    // Create LogTailer and being executing it. The LogTailer will pass new lines to DamageMeter object for processing
+    val listener = new LogTailer(damageMeter);
+    val tailer = Tailer.create(logFile, listener, 1000);
   }
 }
